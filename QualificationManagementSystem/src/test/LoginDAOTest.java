ec276786feca588bc9cc1792c12ee55de4762dd9
yu oishi <mj.se.oy@gmail.com>
2018-07-09 11:58:24 +0900

    LoginDAO用のTestファイルを追加

---
+++ QualificationManagementSystem/src/test/LoginDAOTest.java
@@ -0,0 +1,21 @@
+package test;
+
+import dao.LoginDAO;
+import dto.Login;
+
+public class LoginDAOTest {
+
+	public static void main(String[] args) {
+		Login login = new Login("a", "a", "天野昭");
+		int row = LoginDAO.register(login);
+		if(row > 0){
+			System.out.println("テスト成功");
+		}else{
+			System.out.println("テスト失敗");
+		}
+		Login test = LoginDAO.search("a", "a");
+		System.out.println(test.getId());
+		System.out.println(test.getPw());
+		System.out.println(test.getName());
+	}
+}
