14eac0c8bf3502fe2cb4f1e6a2f9e7dff5886b96
yu oishi <mj.se.oy@gmail.com>
2018-07-09 14:27:30 +0900

    生徒一覧表示追加

--- QualificationManagementSystem/src/dao/StudentDAO.java
+++ QualificationManagementSystem/src/dao/StudentDAO.java
@@ -3,25 +3,34 @@ package dao;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import dto.Student;
 
 public class StudentDAO {
+
+	static final String CLASS = "com.mysql.jdbc.Driver";
+	static final String URL = "jdbc:mysql://localhost:3306/qualification?useSSL=false";
+	static final String USER = "userQ";
+	static final String PASSWORD = "password";
+
+	/*生徒登録処理*/
 	public static int register(Student student){
 		Connection con = null;
 		PreparedStatement pstmt = null;
 		int result = 0;
 
 		try{
-			Class.forName("com.mysql.jdbc.Driver");
-			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/qualification?useSSL=false",
-					"userQ", "password");
-			String sql = "insert into students (no, name, grade) value(?, ?, ?)";
+			Class.forName(CLASS);
+			con = DriverManager.getConnection(URL, USER, PASSWORD);
+			String sql = "insert into students (no, name, grade, class) value(?, ?, ?, ?)";
 			pstmt = con.prepareStatement(sql);
 			pstmt.setString(1, student.getNo());
 			pstmt.setString(2, student.getName());
 			pstmt.setInt(3, student.getGrade());
+			pstmt.setInt(4, student.getClas());
 			result = pstmt.executeUpdate();
 		}catch(ClassNotFoundException e){
 			e.printStackTrace();
@@ -45,5 +54,50 @@ public class StudentDAO {
 			}
 		}
 		return result;
+	}
+
+	/*生徒一覧表示処理*/
+	public static ArrayList<Student> show(){
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		ArrayList<Student> list = new ArrayList<Student>();
+
+		try{
+			Class.forName(CLASS);
+			con = DriverManager.getConnection(URL, USER, PASSWORD);
+			String sql = "select no, name, grade, class from students";
+			pstmt = con.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			while(rs.next()){
+				String no = rs.getString("no");
+				String name = rs.getString("name");
+				int grade = rs.getInt("grade");
+				int clas = rs.getInt("class");
+				Student s = new Student(no, name, grade, clas);
+				list.add(s);
+			}
+		}catch(ClassNotFoundException e){
+			e.printStackTrace();
+		}catch(SQLException e){
+			e.printStackTrace();
+		}finally{
+			try{
+				if(pstmt != null){
+					pstmt.close();
+				}
+			}catch(SQLException e){
+
+			}
+
+			try{
+				if(con != null){
+					con.close();
+				}
+			}catch(SQLException e){
+
+			}
+		}
+		return list;
 	}
 }
--- QualificationManagementSystem/src/test/StudentDAOTest.java
+++ QualificationManagementSystem/src/test/StudentDAOTest.java
@@ -1,5 +1,7 @@
 package test;
 
+import java.util.ArrayList;
+
 import dao.StudentDAO;
 import dto.Student;
 
@@ -12,6 +14,13 @@ public class StudentDAOTest {
 			System.out.println("テスト成功");
 		}else{
 			System.out.println("テスト失敗");
+		}
+		ArrayList<Student> list = StudentDAO.show();
+		for(Student s : list){
+			System.out.print(s.getNo() + " ");
+			System.out.print(s.getName() + " ");
+			System.out.print(s.getGrade() + " ");
+			System.out.print(s.getClas() + " \n");
 		}
 	}
 }
